9b8343be2aaea99f1700188ca01102b736075ba9
Hannah Lee <hanlavlee@gmail.com>
2018-02-18 17:47:46 -0800

    add specs and methods for class List.

--- lib/list.rb
+++ lib/list.rb
@@ -1,5 +1,30 @@
-require("sinatra")
-require("sinatra/reloader")
-also_reload("lib/**/*.rb")
-require("./lib/task")
-require("pg")
+class List
+  attr_reader(:name, :id)
+
+  def initialize(attributes)
+    @name = attributes.fetch(:name)
+    @id = attributes.fetch(:id)
+  end
+
+  def self.all
+    returned_lists = DB.exec("SELECT * FROM lists;")
+    lists = []
+    returned_lists.each() do |list|
+      name = list.fetch("name")
+      id = list.fetch("id").to_i()
+      lists.push(List.new({:name => name, :id => id}))
+    end
+    lists
+  end
+
+  def save
+    result = DB.exec("INSERT INTO lists (name) VALUES ('#{@name}') RETURNING id;")
+    @id = result.first().fetch("id").to_i()
+  end
+  #We can insert a record into the database and have the ID of that new entry be returned to us by adding RETURNING id to the end of our INSERT command.
+  #The pg gem always returns information in an array (technically it's not an array but it behaves more or less like one). When we save a list and want to get its ID, we have to use the first() method to take it out of the array. Then we can use the fetch method to select the ID.
+
+  def ==(another_list)
+    self.name().==(another_list.name()).&(self.id().==(another_list.id()))
+  end
+end
--- spec/list_spec.rb
+++ spec/list_spec.rb
@@ -1,3 +1,35 @@
 require('rspec')
 require('pry')
+require('pg')
 require('list')
+
+DB = PG.connect({:dbname => 'to_do_test'})
+
+RSpec.configure do |config|
+  config.after(:each) do
+    DB.exec("DELETE FROM lists *;")
+  end
+end
+
+describe(List) do
+  describe(".all") do
+    it("starts off with no lists") do
+      expect(List.all()).to(eq([]))
+    end
+  end
+
+  describe("#name") do
+    it("tells you its name") do
+      list = List.new({:name => "Epicodus stuff", :id => nil})
+      expect(list.name()).to(eq("Epicodus stuff"))
+    end
+  end
+
+  describe("#==") do
+    it("is the same list if it has the same name") do
+      list1 = List.new({:name => "Epicodus stuff", :id => nil}) # doesn't have id yet because haven't saved to db; don't know what db will assign it, nil for testing instances.
+      list2 = List.new({:name => "Epicodus stuff", :id => nil})
+      expect(list1).to(eq(list2))
+    end
+  end
+end
