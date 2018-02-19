7150a2c0d51fb6e0d5f22a25ad70e71fc13fa237
Hannah Lee <hanlavlee@gmail.com>
2018-02-18 17:57:11 -0800

    add task specs to test list and tasks tables are connected.

--- spec/task_spec.rb
+++ spec/task_spec.rb
@@ -27,4 +27,26 @@ describe(Task) do
       expect(Task.all()).to(eq([test_task]))
     end
   end
+  
+  describe("#description") do
+  it("lets you read the description out") do
+    test_task = Task.new({:description => "learn SQL", :list_id => 1})
+    expect(test_task.description()).to(eq("learn SQL"))
+  end
+end
+
+describe("#list_id") do
+  it("lets you read the list ID out") do
+    test_task = Task.new({:description => "learn SQL", :list_id => 1})
+    expect(test_task.list_id()).to(eq(1))
+  end
+end
+
+describe("#==") do
+  it("is the same task if it has the same description and list ID") do
+    task1 = Task.new({:description => "learn SQL", :list_id => 1})
+    task2 = Task.new({:description => "learn SQL", :list_id => 1})
+    expect(task1).to(eq(task2))
+  end
+end
 end
