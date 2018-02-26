603d554da0d26b176536e0450ffa5f27d77a7cff
David Lewis and John Rykken <john@>
2018-02-26 10:31:48 -0800

    Finish adding/testing code from curriculum.

--- lib/task.rb
+++ lib/task.rb
@@ -1,2 +1,7 @@
 class Task < ActiveRecord::Base
+  scope(:not_done, -> do
+    where({:done => false})
+  end)
+
+  belongs_to(:list)
 end
--- spec/spec_helper.rb
+++ spec/spec_helper.rb
@@ -4,6 +4,7 @@ require("rspec")
 require("pg")
 require("sinatra/activerecord")
 require("task")
+require("list")
 
 RSpec.configure do |config| # clear db between spec runs
   config.after(:each) do
