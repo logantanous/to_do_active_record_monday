7918cd2cde3ae3bf4be54782c069cb59808c960f
David Lewis and John Rykken <john@>
2018-02-26 09:30:25 -0800

    Copy/paste code from curriculum.

--- app.rb
+++ app.rb
@@ -1,65 +1,32 @@
 require("sinatra")
 require("sinatra/reloader")
 also_reload("lib/**/*.rb")
+require("sinatra/activerecord")
 require("./lib/task")
-require("./lib/list")
 require("pg")
 
-# DB = PG.connect({:dbname => "to_do"}) #connect to NON-TEST db
-
-DB = PG.connect({:dbname => "to_do_test"}) # change back to above when done testing
-
 get('/') do
-  @lists = List.all()
+  @tasks = Task.all
   erb(:index)
 end
 
-get('/lists/new') do
-  erb(:list_form)
+post('/') do
+  task = params[:task]
+  new_task = Task.new(:description => task)
+  new_task.save
+  @tasks = Task.all
+  erb(:index)
 end
 
-post('/lists') do
-  name = params.fetch('name')
-  list = List.new({:name => name, :id => nil})
-  list.save()
-  erb(:success)
+get('/tasks/:id/edit') do
+  @task = Task.find(params.fetch("id").to_i())
+  erb(:task_edit)
 end
 
-get('/lists') do
-  @lists = List.all()
-  erb(:lists)
-end
-
-get("/lists/:id") do # route to view a specific list
-  @list = List.find(params.fetch("id").to_i())
-  erb(:list)
-end
-
-
-post("/tasks") do
+patch("/tasks/:id") do
   description = params.fetch("description")
-  list_id = params.fetch("list_id").to_i()
-  @list = List.find(list_id)
-  @task = Task.new({:description => description, :list_id => list_id})
-  @task.save()
-  erb(:list)
-end
-
-get("/lists/:id/edit") do
-  @list = List.find(params.fetch("id").to_i())
-  erb(:list_edit)
-end
-
-patch("/lists/:id") do
-  name = params.fetch("name")
-  @list = List.find(params.fetch("id").to_i())
-  @list.update({:name => name})
-  erb(:list)
-end
-
-delete("/lists/:id") do
-  @list = List.find(params.fetch("id").to_i())
-  @list.delete()
-  @lists = List.all()
+  @task = Task.find(params.fetch("id").to_i())
+  @task.update({:description => description})
+  @tasks = Task.all()
   erb(:index)
 end
